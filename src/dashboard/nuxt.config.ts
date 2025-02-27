df8eb96430706f3383aea5aa1d68a44268c24a62
Sedat Bali <sedatbali44@gmail.com>
2025-02-27 22:54:49 +0300

    3

--- src/dashboard/nuxt.config.ts
+++ src/dashboard/nuxt.config.ts
@@ -1,12 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            title: 'DataCose: Code Challenge',
-        },
-    },
-    ssr: false,
-    devtools: { enabled: true },
-    pages: true,
-    modules: ['@nuxt/ui'],
+  app: {
+      head: {
+          title: 'DataCose: Code Challenge',
+      },
+  },
+
+  ssr: false,
+  devtools: { enabled: true },
+  pages: true,
+  modules: ['@nuxt/ui'],
+  compatibilityDate: '2025-02-27',
 });
